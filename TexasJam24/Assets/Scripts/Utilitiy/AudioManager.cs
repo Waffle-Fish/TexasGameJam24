c4760e1785a7a7100f02c823ac3ffa5ad63993e6
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-09-28 23:03:43 -0700

    updated settings menu

--- TexasJam24/Assets/Scripts/Utilitiy/AudioManager.cs
+++ TexasJam24/Assets/Scripts/Utilitiy/AudioManager.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("Volume Control")]
     [SerializeField]
     AudioSO audioSettings;
 
+    [Header("Audio Sources")]
     [SerializeField]
     AudioSource jukebox;
-
     List<AudioSource> allSfxSources;
+
+    [Header("Settings Slider")]
+    [SerializeField]
+    Slider musicSlider;
+    [SerializeField]
+    Slider sfxSlider;
+
 
     private void Awake() {
         allSfxSources = GameObject.FindObjectsOfType<AudioSource>().ToList<AudioSource>();
@@ -19,6 +28,19 @@ public class AudioManager : MonoBehaviour
 
         jukebox.volume = audioSettings.MusicVolume;
         
+        if (musicSlider) {
+            // Update slider volume
+            musicSlider.value = audioSettings.MusicVolume;
+            // Update slider handle to proper position
+            musicSlider.handleRect.anchorMin = new(audioSettings.MusicVolume,0f);
+            musicSlider.handleRect.anchorMax = new(audioSettings.MusicVolume,1f);
+        }
+
+        if (sfxSlider) {
+            sfxSlider.value = audioSettings.SfxVolume;
+            sfxSlider.handleRect.anchorMin = new(audioSettings.SfxVolume,0f);
+            sfxSlider.handleRect.anchorMax = new(audioSettings.SfxVolume,1f);
+        }
     }
 
     public void SetMusicVolume() {
@@ -35,10 +57,20 @@ public class AudioManager : MonoBehaviour
     public void UpdateMusicVolume(float vol) {
         vol = Mathf.Clamp(vol, 0f,1f);
         audioSettings.MusicVolume = vol;
+        SetMusicVolume();
     }
 
     public void UpdateSfxVolume(float vol) {
         vol = Mathf.Clamp(vol, 0f,1f);
         audioSettings.SfxVolume = vol;
+        SetSfxVolume();
+    }
+
+    public float GetMusicVolume() {
+        return audioSettings.MusicVolume;
+    }
+
+    public float GetSfxVolume() {
+        return audioSettings.SfxVolume;
     }
 }
--- TexasJam24/Assets/Scripts/Utilitiy/AudioSO.cs
+++ TexasJam24/Assets/Scripts/Utilitiy/AudioSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "AudioSetting", menuName = "Custom/AudioSettings", order = 1)]
 public class AudioSO : ScriptableObject
 {
     [Range(0f, 1f)]
